65a4586358cadd5a5463e491105e0d0297fc5825
Velcori <72639892+Velcori@users.noreply.github.com>
2025-06-07 06:04:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    return {"status": "ok"}, 200
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
     import os
